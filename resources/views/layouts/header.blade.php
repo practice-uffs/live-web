46fca7c719c56175913126f49469c438806b6128
Fernando Bevilacqua <dovyski@gmail.com>
2021-09-08 11:10:19 -0300

    feat: ajuste de UI no cabeçalho

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,11 +9,11 @@
             <ul>
                 @auth
                     @if (@$layout_header_simplified == false)
-                        <li><a href="{{ route('home') }}" class="nav-link @if (Route::is('home')) active @endif" >Minhas criações</a></li>
+                        <li><a href="{{ route('home') }}" class="nav-link @if (Route::is('home')) active @endif" >Minhas transmissões</a></li>
                         <li class="dropdown ml-3">
-                            <a href="{{ route('form.create') }}">
+                            <a href="{{ route('event.create') }}">
                                 <div tabindex="0" class="btn btn-primary">
-                                    Criar
+                                    Transmitir
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>
